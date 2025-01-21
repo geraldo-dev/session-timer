54999030d6cb9c8888f8ee65bd764d51c7929b0b
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-21 19:21:32 -0300

    count sessions

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 let sessions = document.querySelector('#sessions');
+let display_sessions = document.querySelector('.display_sessions');
+let display_times = document.querySelector('.display_times');
 let times = document.querySelector('#times');
 let btn = document.querySelector('#btn');
 
 function start() {
-    console.log(sessions.value, times.value );
+    display_times.textContent = `tempo: ${times.value}`;
+
+    let count_session = Number(sessions.value);
+
+    //count sessions
+    setInterval(()=>{
+        display_sessions.textContent = `sessões: ${count_session}`;
+        if(count_session > 0){
+            count_session--;
+        }
+    }, 1000);
 }
 
 btn.addEventListener('click', start);
