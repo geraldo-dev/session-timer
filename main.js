3203f685b44d61184797c21b7736ac4e5adc99c4
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-23 09:08:45 -0300

    changing minutes and seconds fields and forming output text

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
+let display_times = document.querySelector(".display_times");
+let btn = document.getElementById("btn");
 
-let display_times = document.querySelector('.display_times');
-let times = document.getElementById('times');
-let btn = document.getElementById('btn');
+let min = Number(document.getElementById("min").value);
+let sec = Number(document.getElementById("sec").value);
 
-let second = Number(String(times.value).split(':')[1]);
-let minute = Number(String(times.value).split(':')[0]);
-// let time_total_seconds;
-// let time_total_minutes;
+let second = sec;
+let minute = min;
 
-btn.addEventListener('click',()=>{
+btn.addEventListener("click", () => {
 
-    
-    let duration = Number((minute * 60) + second);
+    let duration = Number(minute * 60 + second);
 
-    //não estao atualizando a variavel
-    
-    setInterval(()=>{
+  //não estao atualizando a variavel
 
+    setInterval( () => {
         let time_total_seconds = parseInt(duration % 60);
         let time_total_minutes = parseInt(duration / 60);
 
+        time_total_minutes = time_total_minutes < 10? `0${time_total_minutes}` :  `${time_total_minutes}`;
+        time_total_seconds = time_total_seconds < 10? `0${time_total_seconds}` :  `${time_total_seconds}`;
+
         display_times.textContent = ` ${time_total_minutes} : ${time_total_seconds} `;
 
-        if(duration > 0){
-            duration --;
+        if (duration > 0) {
+        duration--;
         }
-        
-
     }, 1000);
-    
 });
