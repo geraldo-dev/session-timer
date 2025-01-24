d32d8faaa705b9d31e16a5bd9a9409d3bdaeef68
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-23 21:08:31 -0300

    corrections convert to numeric

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # session-timer
 timer that sets the number of sessions and the time for them
-
--- main.js
+++ main.js
@@ -1,47 +1,32 @@
+let display_sessions =  document.querySelector('.display_sessions');
+let sessions = document.querySelector('#sessions');
 let display_times = document.querySelector(".display_times");
-let btn_start = document.getElementById("btn_start");
-let btn_reset = document.getElementById("btn_reset");
+let minute = document.querySelector("#min");
+let second = document.querySelector("#sec");
 
-let min = document.getElementById("min");
-let sec = document.getElementById("sec");
+var loopInterval;
 
-console.log(sec.value)
 
-let second = Number(sec.value);
-let minute = Number(min.value);
-let loopInterval = undefined;
-
-btn_start.addEventListener("click", () => {
-    
-    let duration = Number(minute * 60 + second);
-
+function session_timer(){
+    let duration =  (Number(minute.value) * 60) + Number(second.value);
 
     loopInterval = setInterval( () => {
-        let time_total_seconds = parseInt(duration % 60);
-        let time_total_minutes = parseInt(duration / 60);
+        let time_total_seconds = parseInt(duration % 60).toFixed(0);
+        let time_total_minutes = parseInt(duration / 60).toFixed(0);
 
-        time_total_minutes = time_total_minutes < 10? `0${time_total_minutes}` :  `${time_total_minutes}`;
-        time_total_seconds = time_total_seconds < 10? `0${time_total_seconds}` :  `${time_total_seconds}`;
+        time_total_minutes = time_total_minutes < 10 ? `0${time_total_minutes}`: `${time_total_minutes}`;
+        time_total_seconds = time_total_seconds < 10 ? `0${time_total_seconds}`: `${time_total_seconds}`;
 
         display_times.textContent = `tempo: ${time_total_minutes}:${time_total_seconds} `;
 
-        if (duration > 0) {
-            duration--;
-        }
-        if(duration < 0){
+        if(duration === 0){
             clearInterval(loopInterval);
         }
+
+        duration--;
         
     }, 1000);
+}
 
-    
-});
-
-btn_reset.addEventListener("click", ()=>{
-    sec.value = '';
-    min.value = '';
-    display_times.textContent = 'tempo: 00:00';
-    clearInterval(loopInterval);
-});
 
 
