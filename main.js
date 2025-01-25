811b3dcb65d4d8bb694f2c5cc8964cea4737ce12
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-25 16:51:24 -0300

    display time format function

--- main.js
+++ main.js
@@ -3,24 +3,53 @@ let sessions = document.querySelector('#sessions');
 let display_times = document.querySelector(".display_times");
 let minute = document.querySelector("#min");
 let second = document.querySelector("#sec");
+let btn_start = document.querySelector('#btn_start');
 
-var loopInterval;
+let session = Number(sessions.value);
+let duration =  (Number(minute.value) * 60) + Number(second.value);
 
 
-function session_timer(){
-    let duration =  (Number(minute.value) * 60) + Number(second.value);
+function formtaTime(time){
+    let res = time < 10 ? `0${time}`: `${time}`;
+    return res;
+}
 
-    loopInterval = setInterval( () => {
-        let time_total_seconds = parseInt(duration % 60).toFixed(0);
-        let time_total_minutes = parseInt(duration / 60).toFixed(0);
 
-        time_total_minutes = time_total_minutes < 10 ? `0${time_total_minutes}`: `${time_total_minutes}`;
-        time_total_seconds = time_total_seconds < 10 ? `0${time_total_seconds}`: `${time_total_seconds}`;
 
-        display_times.textContent = `tempo: ${time_total_minutes}:${time_total_seconds} `;
+function session_timer(time_secunds){
 
-        if(duration === 0){
-            clearInterval(loopInterval);
+    let time_renew = time_secunds;
+    let time_count = time_secunds;
+    
+    //exibir quantidade sessoes na tela
+    display_sessions.textContent = `sessões: ${session}`;
+    
+    let stopInterval = setInterval( () => {
+        
+        let time_total_seconds = parseInt(time_count % 60).toFixed(0);
+        let time_total_minutes = parseInt(time_count / 60).toFixed(0);
+
+        time_total_minutes = formtaTime(time_total_minutes);
+        time_total_seconds = formtaTime(time_total_seconds);
+        
+        if(time_count >= 0){
+            console.log('---', time_total_seconds);
+            display_times.textContent = `tempo: ${time_total_minutes}:${time_total_seconds} `;
+            time_count--;
+
+        }else{
+
+            clearInterval(stopInterval);
+            //retira um da sessõa apois a primeira rodada
+            session--;
+
+            //verifica se ainda a sessões e chama a funcao de novo
+            if(session > 0){
+                session_timer(time_renew);
+            }else{
+                //
+                console.log('acabou as sessoes');
+            }
         }
 
         duration--;
@@ -28,5 +57,8 @@ function session_timer(){
     }, 1000);
 }
 
-
-
+//
+btn_start.addEventListener('click', (e)=> {
+    e.preventDefault();
+    session_timer(duration)
+} );
